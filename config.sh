90c3501d09616c14abd073d86a70704125e40a55
YetiWilson <abaldie@gmail.com>
2019-03-02 09:01:02 -0800

    added update for kali linux

--- config.sh
+++ config.sh
@@ -1,7 +1,11 @@
 sudo apt update
 sudo apt upgrade
-sudo apt install python-pip python-dev libffi-dev libssl-dev
-#from source
+sudo apt install python python-pip python-dev libffi-dev libssl-dev
+# for Kali Linux
+sudo apt install git
+# end for kali linux
+
+# install ansible from source to use developement branches
 git clone https://github.com/daniel-chung-broadcom/ansible.git --recursive
 cd ./ansible
 source ./hacking/env-setup
