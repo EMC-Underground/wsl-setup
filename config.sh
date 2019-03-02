f7fc5fd202738739209e7263b0d81dcc280d511b
YetiWilson <abaldie@gmail.com>
2019-03-02 08:19:42 -0800

    added branch checkout command for brocade modules

--- config.sh
+++ config.sh
@@ -1,9 +1,13 @@
 sudo apt update
 sudo apt upgrade
-sudo apt install python3-pip python3-dev libffi-dev libssl-dev
+sudo apt install python-pip python-dev libffi-dev libssl-dev
 #from source
 git clone https://github.com/daniel-chung-broadcom/ansible.git --recursive
 cd ./ansible
 source ./hacking/env-setup
 sudo pip install -r ./requirements.txt
 # add branch change below
+# change to the brocade-bsn branch to use brocade dev code
+git checkout brocade_bsn
+# check the current branch
+git branch
